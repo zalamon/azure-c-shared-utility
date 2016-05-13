f3b3d632821904229f5072ef7a28d5383fea9ffc
Marc Schier <mschier@microsoft.com>
2016-05-13 14:00:14 +0200

    Condition on win32 with wait counter

--- adapters/condition_win32.c
+++ adapters/condition_win32.c
@@ -10,24 +10,37 @@
 #include "azure_c_shared_utility/condition.h"
 #include "windows.h"
 #include "azure_c_shared_utility/iot_logging.h"
+#include "azure_c_shared_utility/gballoc.h"
 
 DEFINE_ENUM_STRINGS(COND_RESULT, COND_RESULT_VALUES);
+
+typedef struct CONDITION_TAG
+{
+    volatile LONG waiting_thread_count;
+    HANDLE event_handle;
+}
+CONDITION;
 
 COND_HANDLE Condition_Init(void)
 {
     // Codes_SRS_CONDITION_18_002: [ Condition_Init shall create and return a CONDITION_HANDLE ]
 
-    HANDLE* cond = (HANDLE*)malloc(sizeof(HANDLE));
+    CONDITION* cond = (CONDITION*)malloc(sizeof(CONDITION));
 
     // Codes_SRS_CONDITION_18_008: [ Condition_Init shall return NULL if it fails to allocate the CONDITION_HANDLE ]
     if (cond != NULL)
     {
-        *cond = CreateEvent(NULL, FALSE, FALSE, NULL);
+        cond->event_handle = CreateEvent(NULL, FALSE, FALSE, NULL);
 
-        if (*cond == INVALID_HANDLE_VALUE)
+        if (cond->event_handle == INVALID_HANDLE_VALUE)
         {
             free(cond);
             cond = NULL;
+        }
+        else
+        {
+            /* Needed to emulate pthread_signal as we only signal the event when there are waiting threads */
+            cond->waiting_thread_count = 0;
         }
     }
 
@@ -44,7 +57,10 @@ COND_RESULT Condition_Post(COND_HANDLE handle)
     }
     else
     {
-        if (SetEvent(*(HANDLE*)handle))
+        CONDITION* cond = (CONDITION*)handle;
+
+        /* Emulate pthreads signalling, by only unblocking *one* waiting thread if there is one waiting */
+        if (cond->waiting_thread_count == 0 || SetEvent(cond->event_handle))
         {
             // Codes_SRS_CONDITION_18_003: [ Condition_Post shall return COND_OK if it succcessfully posts the condition ]
             result = COND_OK;
@@ -60,6 +76,7 @@ COND_RESULT Condition_Post(COND_HANDLE handle)
 COND_RESULT Condition_Wait(COND_HANDLE handle, LOCK_HANDLE lock, int timeout_milliseconds)
 {
     COND_RESULT result;
+
     // Codes_SRS_CONDITION_18_004: [ Condition_Wait shall return COND_INVALID_ARG if handle is NULL ]
     // Codes_SRS_CONDITION_18_005: [ Condition_Wait shall return COND_INVALID_ARG if lock is NULL and timeout_milliseconds is 0 ]
     // Codes_SRS_CONDITION_18_006: [ Condition_Wait shall return COND_INVALID_ARG if lock is NULL and timeout_milliseconds is not 0 ]
@@ -69,34 +86,42 @@ COND_RESULT Condition_Wait(COND_HANDLE handle, LOCK_HANDLE lock, int timeout_mil
     }
     else
     {
+        CONDITION* cond = (CONDITION*)handle;
         DWORD wait_result;
 
-        if (timeout_milliseconds == 0)
-        {
-            timeout_milliseconds = INFINITE;
-        }
-
-        Unlock(lock);
+        /* Increment the waiting thread count, unlock the lock and wait */
+        InterlockedIncrement(&cond->waiting_thread_count);
+        
+        (void)Unlock(lock);
 
         // Codes_SRS_CONDITION_18_013: [ Condition_Wait shall accept relative timeouts ]
-        wait_result = WaitForSingleObject(*(HANDLE*)handle, timeout_milliseconds);
+        wait_result = WaitForSingleObject(cond->event_handle, timeout_milliseconds == 0 ? INFINITE : timeout_milliseconds);
 
-        Lock(lock);
+        (void)Lock(lock);
 
-        if (wait_result == WAIT_TIMEOUT)
+        if (wait_result != WAIT_OBJECT_0 && wait_result != WAIT_TIMEOUT)
         {
-            // Codes_SRS_CONDITION_18_011: [ Condition_Wait shall return COND_TIMEOUT if the condition is NOT triggered and timeout_milliseconds is not 0 ]
-            result = COND_TIMEOUT;
-        }
-        else if (wait_result == WAIT_OBJECT_0)
-        {
-            // Codes_SRS_CONDITION_18_012: [ Condition_Wait shall return COND_OK if the condition is triggered and timeout_milliseconds is not 0 ]
-            result = COND_OK;
+            /* cond might be freed at this point, just return error */
+            result = COND_ERROR;
         }
         else
         {
-            LogError("Failed to Condition_Wait");
-            result = COND_ERROR;
+            /* To handle the chance of a race condition reset the event again when there are no more waiting threads */
+            if (InterlockedDecrement(&cond->waiting_thread_count) == 0)
+            {
+                (void)ResetEvent(cond->event_handle);
+            }
+
+            if (wait_result == WAIT_TIMEOUT)
+            {
+                // Codes_SRS_CONDITION_18_011: [ Condition_Wait shall return COND_TIMEOUT if the condition is NOT triggered and timeout_milliseconds is not 0 ]
+                result = COND_TIMEOUT;
+            }
+            else
+            {
+                // Codes_SRS_CONDITION_18_012: [ Condition_Wait shall return COND_OK if the condition is triggered and timeout_milliseconds is not 0 ]
+                result = COND_OK;
+            }
         }
     }
     return result;
@@ -105,11 +130,15 @@ COND_RESULT Condition_Wait(COND_HANDLE handle, LOCK_HANDLE lock, int timeout_mil
 void Condition_Deinit(COND_HANDLE handle)
 {
     // Codes_SRS_CONDITION_18_007: [ Condition_Deinit will not fail if handle is NULL ]
+    // Codes_SRS_CONDITION_18_009: [ Condition_Deinit will deallocate handle if it is not NULL 
     if (handle != NULL)
     {
-        // Codes_SRS_CONDITION_18_009: [ Condition_Deinit will deallocate handle if it is not NULL 
-        CloseHandle(*(HANDLE*)handle);
-        free(handle);
+        CONDITION* cond = (CONDITION*)handle;
+
+        (void)CloseHandle(cond->event_handle);
+        cond->event_handle = INVALID_HANDLE_VALUE;
+
+        free(cond);
     }
 }
 
