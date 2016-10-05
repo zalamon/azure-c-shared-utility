983cb4aaa4ce0884b027ea7d38d02e0f02b3596c
dcristoloveanu <dcristo@microsoft.com>
2016-10-04 17:05:05 -0700

    Add unittests

--- tests/tlsio_cyclonessl_socket_bsd_ut/main.c
+++ tests/tlsio_cyclonessl_socket_bsd_ut/main.c
@@ -6,6 +6,6 @@
 int main(void)
 {
     size_t failedTestCount = 0;
-    RUN_TEST_SUITE(tlsio_cyclonessl_socket_win32, failedTestCount);
+    RUN_TEST_SUITE(tlsio_cyclonessl_socket_bsd, failedTestCount);
     return failedTestCount;
 }
