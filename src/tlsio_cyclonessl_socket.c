16dabd302053393e02e0ad00a02be397473a7da7
dcristoloveanu <dcristo@microsoft.com>
2016-10-04 17:49:03 -0700

    Add cyclonessl_socket fiels

---
+++ src/tlsio_cyclonessl_socket.c
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdlib.h>
+#include "azure_c_shared_utility/xlogging.h"
+#include "socket.h"
+#include "tls.h"
+
+TlsSocket tlsio_cyclonessl_socket_create(const char* hostname, int port)
+{
+    TlsSocket result;
+    if (hostname == NULL)
+    {
+        LogError("NULL hostname");
+        result = NULL;
+    }
+    else
+    {
+        Socket* socket = socketOpen(SOCKET_TYPE_STREAM, SOCKET_IP_PROTO_TCP);
+        if (!socket)
+        {
+            LogError("socketOpen failed, cannot create socket");
+            result = NULL;
+        }
+        else
+        {
+            IpAddr ipAddr;
+
+            if (getHostByName(NULL, hostname, &ipAddr, 0))
+            {
+                socketClose(socket);
+                LogError("Cannot resolve host");
+                result = NULL;
+            }
+            else if (socketConnect(socket, &ipAddr, port))
+            {
+                socketClose(socket);
+                LogError("Failed to connect");
+                result = NULL;
+            }
+            else
+            {
+                result = (TlsSocket)socket;
+            }
+        }
+    }
+
+    return result;
+}
+
+void tlsio_cyclonessl_socket_destroy(TlsSocket socket)
+{
+    if (socket == NULL)
+    {
+        LogError("tlsio_cyclonessl_socket_destroy: NULL socket");
+    }
+    else
+    {
+        socketClose(socket);
+    }
+}
