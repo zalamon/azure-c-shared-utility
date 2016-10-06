bd26b9af932c0db824a16a583e7c5f0a6655a045
dcristoloveanu <dcristo@microsoft.com>
2016-10-05 22:20:51 -0700

    Fix some test code

--- tests/tlsio_cyclonessl_socket_bsd_ut/tlsio_cyclonessl_socket_bsd_ut.c
+++ tests/tlsio_cyclonessl_socket_bsd_ut/tlsio_cyclonessl_socket_bsd_ut.c
@@ -12,6 +12,21 @@
 
 #include "tls.h"
 
+#undef htonl
+#undef htons
+#undef ntohl
+#undef ntohs
+
+#if _WIN32
+#undef DECLSPEC_IMPORT
+
+#pragma warning(disable: 4273 4189)
+
+#include <winsock2.h>
+#include <mstcpip.h>
+#include <ws2tcpip.h>
+#endif
+
 #include <stddef.h>
 
 #include "testrunnerswitcher.h"
@@ -36,6 +51,22 @@ void my_gballoc_free(void* ptr)
 #define ENABLE_MOCKS
 
 #include "azure_c_shared_utility/gballoc.h"
+
+SOCKET TEST_SOCKET = (SOCKET)0x4243;
+struct hostent* TEST_HOST_ENT = (struct hostent*)0x4244;
+
+MOCK_FUNCTION_WITH_CODE(WSAAPI, SOCKET, socket, int, af, int, type, int, protocol)
+MOCK_FUNCTION_END(TEST_SOCKET)
+MOCK_FUNCTION_WITH_CODE(WSAAPI, int, closesocket, SOCKET, s)
+MOCK_FUNCTION_END(0)
+MOCK_FUNCTION_WITH_CODE(WSAAPI, int, connect, SOCKET, s, const struct sockaddr*, name, int, namelen)
+MOCK_FUNCTION_END(0)
+MOCK_FUNCTION_WITH_CODE(WSAAPI, int, WSAGetLastError)
+MOCK_FUNCTION_END(0)
+MOCK_FUNCTION_WITH_CODE(WSAAPI, struct hostent*, gethostbyname, const char*, name);
+MOCK_FUNCTION_END(TEST_HOST_ENT)
+MOCK_FUNCTION_WITH_CODE(WSAAPI, INT, getaddrinfo, PCSTR, pNodeName, PCSTR, pServiceName, const ADDRINFOA*, pHints, PADDRINFOA*, ppResult)
+MOCK_FUNCTION_END(0)
 
 #undef ENABLE_MOCKS
 
@@ -96,10 +127,12 @@ TEST_FUNCTION_CLEANUP(TestMethodCleanup)
 /* tlsio_cyclonessl_socket_create */
 
 /* Tests_SRS_TLSIO_CYCLONESSL_SOCKET_BSD_01_001: [ tlsio_cyclonessl_socket_create shall create a new socket to be used by CycloneSSL. ]*/
-TEST_FUNCTION(tickcounter_create_fails)
+TEST_FUNCTION(tlsio_cyclonessl_socket_create_succeeds)
 {
     ///arrange
     TlsSocket socket;
+
+    STRICT_EXPECTED_CALL(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
 
     ///act
     int result = tlsio_cyclonessl_socket_create("testhostname", 4242, &socket);
